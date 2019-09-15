b49856df7d9676a86f31b2c31b4d98ac65ede973
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-15 02:54:24 +0200

    3.8: Create New Pages in Rails
    - install Yarn
    - fix installation of Webpacker
    - add new controller `simple pages`
    - add new pages: `index`, `about`, `contact`
    - add contact to pages
    - add path of new pages to `routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'simple_pages/index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  root 'simple_pages#index' #set index.html as site's home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
