3d079f6e5e7790c79396caf9bf6c379ff3874dd5
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-19 19:49:02 +0200

    5.2 Forms
    - change back redirect from edit product to showing individual product
    - change back root to `landing_page`
    - put `search` box into navbar
    - define list of filtered items in `product.rb`, use 2 different matching operators depending on environment (development/SQL or production/Postgres )
    - style form for new and update product:
      - change background colour of text fields
      - change look of button
      - tidy up vertical alignment
    - style search form
      - change look of button
      - change background colour of text fields
      - remove text above
      - put search box and search button on one line

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'products#index' #set products/index.html.erb as site's home page
+  root 'simple_pages#landing_page' #set products/index.html.erb as site's home page
   # root 'simple_pages#landing_page' #set landing_page.html.erb as site's home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
