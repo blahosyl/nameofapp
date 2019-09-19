794c9705b049075a9bc1a96fc94e40f8575bf42c
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-19 21:03:08 +0200

    5.3. Mailer start
    - out contact form on "Contact" page, add it to `routes.rb`
    - create "Thank you" page to be shown after submitting contact form
    - define "thank_you" method for mailer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  post 'simple_pages/thank_you' #contact form
   root 'simple_pages#landing_page' #set index.html as site's home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
