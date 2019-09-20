113fd0d71121f3eba9566e32b891472cca8f237a
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-20 17:06:37 +0200

    5.3. Sending Emails pt. 2
    - move definition of `thank_you` email from `application_mailer.rb` to `simple_pages_controller.rb`
    - enable SendGrid from Heroku (`config/environment.rb`

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,4 +8,15 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.limit(4)
   end
+
+  # send automatic message based on data from contact from
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+    end
 end
