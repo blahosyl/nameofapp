e38a9cb02a4b964ca2885896588226c4d48d095f
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-20 22:34:08 +0200

    5.3: Sending Emails: fix `thank_you` definition

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -14,8 +14,8 @@ class SimplePagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
+    ActionMailer::Base.mail(from: "sylvia.blaho@gmail.com",
+      to: @email,
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
     end
