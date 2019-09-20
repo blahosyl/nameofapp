8e6ad7da24688766b23ac3f2289295e049f98214
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-21 00:10:25 +0200

    5.3 Mailer
    - switch `to` and `from` in `thank_you` email sender

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -14,8 +14,8 @@ class SimplePagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: "sylvia.blaho@gmail.com",
-      to: @email,
+    ActionMailer::Base.mail(from: @email,
+      to: "sylvia.blaho@gmail.com",
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
     end
