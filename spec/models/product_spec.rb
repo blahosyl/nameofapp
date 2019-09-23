569555e7ecb9a7aae7c0abb13d566687668ecbc4
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-23 02:03:46 +0200

    fix test for avg rating

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe Product do
+describe Product
   let(:product) { Product.create!(name: "race bike") }
   let(:user) { User.create!(name: "Test User") }
-end
 
-before do
-  product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-  product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-  product.comments.create!(rating: 5, user: user, body: "Great bike!")
-end
+   before do
+    product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+    product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+    product.comments.create!(rating: 5, user: user, body: "Great bike!")
+  end
 
-it "returns the average rating of all comments" do
-  expect(average_rating).to eq 3
+  it "returns the average rating of all comments" do
+    expect(average_rating).to eq 3
+  end
 end
