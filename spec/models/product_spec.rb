b8d851c40ba7ef49f22990bf9e6c6c21390cfeb3
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-23 22:10:46 +0200

    6.2: Unit Tests
    - add validation for product price and name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,10 +12,28 @@ describe Product do
       product.comments.create!(rating: 3, user: user, body: "Ok bike!")
       product.comments.create!(rating: 5, user: user, body: "Great bike!")
 
-      end
+    end
 
-      it "returns the average rating of all comments" do
-        expect(product.average_rating).to eq 3
+    it "returns the average rating of all comments" do
+      expect(product.average_rating).to eq 3
+    end
+  end
+
+  # product created without a name should not be valid
+  context "when product gets created" do
+    let(:product) { Product.new(name: nil, description: "anything", price: 12, image_url: "anything") }
+
+    it "is not valid without a name" do
+      expect(Product.new).not_to be_valid
+    end
+  end
+
+  # product created without a price should not be valid
+  context "when product gets created" do
+    let(:product) { Product.new(name: "anything", description: "anything", price: nil, image_url: "anything") }
+
+    it "is not valid without a price" do
+      expect(Product.new).not_to be_valid
     end
   end
 end
