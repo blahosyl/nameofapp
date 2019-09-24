9d691a9e5bf39f49845e81c7d61b5a2232feb51e
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-24 10:34:39 +0200

    6.6: Creating Payments
    - generate `payments` controller
    - define `create` method in payments controller:
      - define `@product` variable as current product
      - define `@user` variable as current user
      - require login before payment
      - change currency to `eur`
      - add user email as `receipt_email`
      -  add redirect to product page, with notice
      - add error notification
    - add conditional to `payments_controller`: when paid, create order
    Problem: Stripe integrated, but product cannot be found

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'payments/create'
   devise_for :users
   resources :users
   resources :products do
     resources :comments
+    resources :payments
   end
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   post 'simple_pages/thank_you' #contact form
+  post 'payments/create' #new payment
   root 'simple_pages#landing_page' #set index.html as site's home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
