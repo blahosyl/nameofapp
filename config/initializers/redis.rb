cf1ab739f82c37c11885f8d8b210c7d74d7e6463
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-25 18:26:59 +0200

    6.12 Redis
    - add conditional for local and production environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis.new(host: 'localhost', port: 6379)
+if Rails.env.production?
+  $redis = Redis.new(host: ENV["REDIS_URL"])
+else
+  $redis = Redis.new(host: 'localhost', port: 6379)
+end
