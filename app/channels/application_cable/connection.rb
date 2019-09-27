423c8e2a7b651dd82fed35738507363638655343
Sylvia Blaho <sylvia.blaho@n26.com>
2019-09-27 18:08:10 +0200

    Update connection.rb

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,24 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+  identified_by :current_user
 
-    def connect
-      self.current_user = find_verified_user
+ def connect
+    self.current_user = find_verified_user
+    logger.add_tags 'ActionCable', current_user.email
+  end
+
+  protected
+
+  def find_verified_user
+    if verified_user == env['warden'].user
+     verified_user
+    else
+      reject_unauthorized_connection
     end
-
-    def disconnect
-
-    end
-
-    protected
-
-      def find_verified_user
-        if verified_user = request.env['warden'].user
-          verified_user
-        else
-          reject_unauthorized_connection
-        end
-      end
-
   end
 end
